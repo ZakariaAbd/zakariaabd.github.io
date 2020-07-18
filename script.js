ed99001aee434b46597452b3d42fd7725e8ba9fc
ZakariaAbd <65509346+ZakariaAbd@users.noreply.github.com>
2020-07-18 13:06:50 -0600

    Commit

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 const btn = document.getElementById('bruh');
-const input = document.getElementById('input');
-const alerts = document.getElementById('alerts');
 
 btn.addEventListener('click', function () {
     if (true) {
